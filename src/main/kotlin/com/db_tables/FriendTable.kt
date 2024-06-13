de8739385c71ac6e2030812d855e22dc96092ac4
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-06-13 12:01:28 +0300

    - delete friends when the account deleted bug fixed

--- src/main/kotlin/com/db_tables/FriendTable.kt
+++ src/main/kotlin/com/db_tables/FriendTable.kt
@@ -1,13 +1,16 @@
 package com.db_tables
 
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
 object FriendTable : Table() {
     val id = integer("id").autoIncrement()
-    val userEmail = varchar("user_email", 128).references(UserTable.email)
-    val friendEmail = varchar("friend_email", 128).references(UserTable.email)
+    val userEmail = varchar("user_email", 128).references(UserTable.email, onDelete = ReferenceOption.CASCADE)
+    val friendEmail = varchar("friend_email", 128).references(UserTable.email, onDelete = ReferenceOption.CASCADE)
     val friendUsername = varchar("friend_username", 128)
     val friendProfilePicUrl = varchar("friend_prof_pic_url", 256).nullable()
+
+    override val primaryKey = PrimaryKey(id)
 
     init {
         uniqueIndex(userEmail, friendEmail)
