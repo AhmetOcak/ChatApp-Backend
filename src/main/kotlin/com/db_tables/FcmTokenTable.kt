ed72d59bb8b46975c906aef6afe70caf8dc6eac9
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-06-23 12:47:35 +0300

    - update fcm token bug fixed

--- src/main/kotlin/com/db_tables/FcmTokenTable.kt
+++ src/main/kotlin/com/db_tables/FcmTokenTable.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.Table
 object FcmTokenTable : Table() {
     val id = integer("id").autoIncrement()
     val email = varchar("email", 128).references(UserTable.email, onDelete = ReferenceOption.CASCADE)
-    val token = varchar("token", 256).uniqueIndex()
+    val token = varchar("token", 256)
 
     override val primaryKey = PrimaryKey(id)
 }
