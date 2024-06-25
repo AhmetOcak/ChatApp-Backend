88c9a83288fc2f3156e119d21965247919a9856c
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-06-25 20:06:19 +0300

    - service_account json file path changed

--- src/main/kotlin/com/firebase/FirebaseAdmin.kt
+++ src/main/kotlin/com/firebase/FirebaseAdmin.kt
@@ -3,10 +3,11 @@ package com.firebase
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import java.io.FileInputStream
 import java.io.InputStream
 
 object FirebaseAdmin {
-    private val service : InputStream? = this::class.java.classLoader.getResourceAsStream("/etc/secrets/service_account.json")
+    private val service : InputStream = FileInputStream("/etc/secrets/service_account.json")
 
     private val options: FirebaseOptions = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(service))
